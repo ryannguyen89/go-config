5b789b324a710d9cc7375ccdb544964b71f6b79a
Hoang Nguyen <hoangnn3007@gmail.com>
2019-08-28 17:49:33 +0700

    Add config.
    Signed-off-by: Hoang Nguyen <hoangnn3007@gmail.com>

---
+++ config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
+
+const (
+	defaultConfigFile = "config.yaml"
+	defaultPath = "conf/"
+)
+
+func init() {
+	var (
+		runMode = os.Getenv("RUNMODE")
+		configFile string
+	)
+
+	log.Printf("RUNMODE is: %s\n", runMode)
+	if len(runMode) > 0 {
+		configFile = fmt.Sprintf("%s.%s", runMode, defaultConfigFile)
+	} else {
+		configFile = defaultConfigFile
+	}
+	configFile = defaultPath + configFile
+
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(configFile)
+
+	// Read config file
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Printf("Read config file error: %v\n", err)
+	}
+	viper.AutomaticEnv()
+}
+
+func Str(key string) string {
+	return viper.GetString(key)
+}
+
+func Int(key string) int {
+	return viper.GetInt(key)
+}
+
+func Int32(key string) int32 {
+	return viper.GetInt32(key)
+}
