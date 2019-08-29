3caed83c7f69d0215cf7bb789839a221d6254f7b
Hoang Nguyen <hoangnn89@outlook.com>
2019-08-29 10:19:57 +0700

    Support configure runmode env.
    Signed-off-by: Hoang Nguyen <hoangnn89@outlook.com>

--- config.go
+++ config.go
@@ -13,17 +13,72 @@ const (
 	defaultConfigFile = "config.yaml"
 	defaultPath = "conf/"
 	defaultType = "yaml"
+	defaultRunModeEnv = "RUNMODE"
 )
 
 var (
 	configPath string
 	configFile string
 	configType string
+	configRunModeEnv string
 	configInstance *hConfig
 	configOnce sync.Once
 )
 
 type hConfig struct {}
+
+func getInstance() *hConfig {
+	configOnce.Do(func() {
+		configInstance = initializeConfigInstance()
+	})
+
+	return configInstance
+}
+
+func initializeConfigInstance() *hConfig {
+	var (
+		runMode string
+		processedConfigFile string
+	)
+
+	// Set default config values
+	if len(configPath) == 0 {
+		configPath = defaultPath
+	}
+	if len(configFile) == 0 {
+		configFile = defaultConfigFile
+	}
+	if len(configType) == 0 {
+		configType = defaultType
+	}
+	if len(configRunModeEnv) == 0 {
+		configRunModeEnv = defaultRunModeEnv
+	}
+
+	// Get run mode
+	runMode = os.Getenv(configRunModeEnv)
+	log.Printf("RUNMODE is: %s\n", runMode)
+
+	// Get config file
+	if len(runMode) > 0 {
+		processedConfigFile = fmt.Sprintf("%s.%s", runMode, configFile)
+	} else {
+		processedConfigFile = configFile
+	}
+	processedConfigFile = configPath + processedConfigFile
+
+	viper.SetConfigType(configType)
+	viper.SetConfigFile(processedConfigFile)
+
+	// Read config file
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Printf("Read config file error: %v\n", err)
+	}
+	viper.AutomaticEnv()
+
+	return &hConfig{}
+}
 
 func (h *hConfig) Str(key string) string {
 	value := viper.GetString(key)
@@ -67,49 +122,10 @@ func (h *hConfig) Debug() {
 	viper.Debug()
 }
 
-func getInstance() *hConfig {
-	configOnce.Do(func() {
-		configInstance = initializeConfigInstance()
-	})
-
-	return configInstance
+func (h *hConfig) getRunMode() string {
+	return viper.GetString(configRunModeEnv)
 }
 
-func initializeConfigInstance() *hConfig {
-	var (
-		runMode              = os.Getenv("RUNMODE")
-		processedConfigFile string
-	)
-	log.Printf("RUNMODE is: %s\n", runMode)
-
-	if len(configPath) == 0 {
-		configPath = defaultPath
-	}
-	if len(configFile) == 0 {
-		configFile = defaultConfigFile
-	}
-	if len(configType) == 0 {
-		configType = defaultType
-	}
-	if len(runMode) > 0 {
-		processedConfigFile = fmt.Sprintf("%s.%s", runMode, configFile)
-	} else {
-		processedConfigFile = configFile
-	}
-	processedConfigFile = configPath + processedConfigFile
-
-	viper.SetConfigType(configType)
-	viper.SetConfigFile(processedConfigFile)
-
-	// Read config file
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Printf("Read config file error: %v\n", err)
-	}
-	viper.AutomaticEnv()
-
-	return &hConfig{}
-}
 
 func SetFile(file string) {
 	configFile = file
@@ -123,9 +139,18 @@ func SetType(_type string) {
 	configType = _type
 }
 
+func SetRunModeEnv(env string) {
+	configRunModeEnv = env
+}
+
 func Get(key string) interface{} {
 	return getInstance().Get(key)
 }
+
+func GetRunMode() string {
+	return getInstance().getRunMode()
+}
+
 func Str(key string) string {
 	return getInstance().Str(key)
 }
--- examples/main.go
+++ examples/main.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	config.SetPath("../conf/")
+	config.SetRunModeEnv("MODE")
 	config.Debug()
 	log.Printf(config.Str("FOO"))
 	log.Println(config.Str("OBJECT_FIELD1"))
+	log.Println(config.GetRunMode())
 }
